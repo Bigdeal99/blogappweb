57ae4137693e85ea25d846fa689e57e857c2dec4
Marcelo Hani <90682271+Bigdeal99@users.noreply.github.com>
2023-12-20 17:23:59 +0100

    ses

--- infrastructure/obj/Release/net7.0/infrastructure.AssemblyInfo.cs
+++ infrastructure/obj/Release/net7.0/infrastructure.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("infrastructure")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Release")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+5aa46612bc507599c9c78c9afa067a459071f5f8")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+1731027283bcc7a37642823fd729ba25d97db1bc")]
 [assembly: System.Reflection.AssemblyProductAttribute("infrastructure")]
 [assembly: System.Reflection.AssemblyTitleAttribute("infrastructure")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
