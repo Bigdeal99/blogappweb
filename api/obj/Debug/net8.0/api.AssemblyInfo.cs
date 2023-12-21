3a8199eb9fd25909117f7dcf775de024569d64bb
Marcelo Hani <90682271+Bigdeal99@users.noreply.github.com>
2023-12-21 01:06:40 +0100

    d

--- api/obj/Debug/net8.0/api.AssemblyInfo.cs
+++ api/obj/Debug/net8.0/api.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("api")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+fce1c7aa785b8a5da7cea1ed73a45d4277044f62")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+9f8c1d4412cc3291d544c4b5a7d1f2d7a2b2c582")]
 [assembly: System.Reflection.AssemblyProductAttribute("api")]
 [assembly: System.Reflection.AssemblyTitleAttribute("api")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
